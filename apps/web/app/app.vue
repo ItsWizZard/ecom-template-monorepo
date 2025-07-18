ed80875c6d622638f6b9d9f75aefaeae9e3c7640
WizZard <96141684+ItsWizZard@users.noreply.github.com>
2025-07-18 08:19:37 +0100

    chore: format

--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-  const { data: users } = await useAsyncData("users", () =>
-    $fetch("http://localhost:3000/api/users", { method: "get" })
-  );
-  const { user, login, logout } = useUser();
+const { data: users } = await useAsyncData('users', () =>
+  $fetch('http://localhost:3000/api/users', { method: 'get' }),
+);
+const { user, login, logout } = useUser();
 </script>
 
 <template>
@@ -14,54 +14,53 @@
       <button @click="logout">Logout</button>
     </div>
 
-    <pre>User: {{ user || "Not logged in" }}</pre>
+    <pre>User: {{ user || 'Not logged in' }}</pre>
     <pre>Fetched Users: {{ users }}</pre>
   </div>
 </template>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-  .container {
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    gap: 1em;
+.container {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
 
-    color: #f7f7f7;
-    background-color: #101010;
-  }
+  color: #f7f7f7;
+  background-color: #101010;
+}
 
-  h1 {
-    margin: 0 auto;
-    margin-top: 1em;
-    width: 60%;
+h1 {
+  margin: 0 auto;
+  margin-top: 1em;
+  width: 60%;
 
-    text-align: center;
-  }
+  text-align: center;
+}
 
-  .buttons {
-    margin: 0 auto;
-    display: flex;
-    gap: 1em;
-  }
+.buttons {
+  margin: 0 auto;
+  display: flex;
+  gap: 1em;
+}
 
-  button {
-    padding: 0.25em 0.5em;
-  }
+button {
+  padding: 0.25em 0.5em;
+}
 
-  pre {
-    margin: 0 auto;
-    width: 80%;
-    min-height: 30px;
+pre {
+  margin: 0 auto;
+  width: 80%;
+  min-height: 30px;
 
-    padding: 0.5em 0.75em;
-    border-radius: 0.35em;
-    background-color: #ffffff19;
-  }
+  padding: 0.5em 0.75em;
+  border-radius: 0.35em;
+  background-color: #ffffff19;
+}
 </style>
-
--- apps/web/app/composables/useUser.ts
+++ apps/web/app/composables/useUser.ts
@@ -1,11 +1,11 @@
-import type { User } from "@ecom-template/types";
+import type { User } from '@ecom-template/types';
 
 const user = ref<User | null>(null);
 
 export function useUser() {
   const login = async () => {
-    const result = await $fetch<User>("http://localhost:3000/api/login", {
-      method: "post",
+    const result = await $fetch<User>('http://localhost:3000/api/login', {
+      method: 'post',
       body: {},
     });
 
