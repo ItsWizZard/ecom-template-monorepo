e49b37ceac179099f029350416a9fdd0614267d7
WizZard <96141684+ItsWizZard@users.noreply.github.com>
2025-07-18 06:08:41 +0100

    fix (server): type import

--- apps/server/server/api/users.ts
+++ apps/server/server/api/users.ts
@@ -1,16 +1,16 @@
-import { User } from "@ecom-template/types";
+import type { User } from '@ecom-template/types';
 
 export default defineEventHandler(async (event) => {
   const users: User[] = [
     {
-      id: "1",
-      email: "bob@test.com",
-      password: "bob123",
+      id: '1',
+      email: 'bob@test.com',
+      password: 'bob123',
     },
     {
-      id: "2",
-      email: "alice@test.com",
-      password: "alice123",
+      id: '2',
+      email: 'alice@test.com',
+      password: 'alice123',
     },
   ];
 
